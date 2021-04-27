991b2bec5138d02eef43f1836d4c33922eb13dae
Juan David Payán <JuanDavidPF@gmail.com>
2021-04-27 15:31:30 -0500

    Added pair selection

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ var selecciona = document.querySelector(".selecciona");
 let arregloDeLista;
 let celda;
 let fila;
+
+let peopleSelected = [];
+
 //Cargar el archivo
 
 $.ajax({
   url: "./datos_sprint2.csv",
-  dataType: "text"
+  dataType: "text",
 }).done(successFunction);
 
 function successFunction(data) {
@@ -19,127 +22,109 @@ function successFunction(data) {
   var datosFila = data.split("\n");
   //Arreglo donde se guarda la nueva información
   var informacion = [];
-  
+
   for (let index = 1; index < datosFila.length; index++) {
     //Lectura de una linea
     let dataLinea = datosFila[index];
-    
+
     //Creacion de fila Crea la tabla
     fila = document.createElement("tr");
-    
+
     //Division por ;
     arregloDeLista = dataLinea.split(",");
-    
+
     for (let indice = 0; indice < arregloDeLista.length; indice++) {
       //Lectura de celda
       let dataCelda = arregloDeLista[indice];
-      
+
       celda = document.createElement("td");
       celda.innerHTML = dataCelda;
-      
+
       fila.appendChild(celda);
     }
-    
+
     table.appendChild(fila);
     //AQUI SE LE AGREGAN POSICIONES
-    informacion.push({correo:arregloDeLista[0],participaciones:arregloDeLista[1], columnaa:arregloDeLista[2], columnab: arregloDeLista[3]});
-    
-    
-  }
-  console.log("Archivo Original");
-  console.log(informacion);
-  btnOrdenarA.addEventListener('click',ordenarA );
-  
-  btnOrdenarB.addEventListener('click',ordenarB );
-  
-  function ordenarA(){
-    if( selecciona.value === "participaciones"){
-      console.log("Participantes ordenado de forma Ascendente");
-      informacion.sort(function(objetoA, objetoB)
-      { 
-        
-        //Ordenar por ID
-        return objetoA.participaciones - objetoB.participaciones;
-        
-        
-      });
-      
-      console.log(informacion);
+    informacion.push({
+      correo: arregloDeLista[0],
+      participaciones: arregloDeLista[1],
+      columnaa: arregloDeLista[2],
+      columnab: arregloDeLista[3],
+    });
 
-      
-    }
-    else if( selecciona.value === "columnaA"){
-      console.log("Columna A ordenado de forma Ascendente");
-      informacion.sort(function(objetoA, objetoB)
-      { 
-        
-        //Ordenar por ID
-        return objetoA.columnaa - objetoB.columnaa;
-        
-        
-      });
-      console.log(informacion);
-      
-      
-    }
-    else if( selecciona.value === "columnaB"){
-      console.log("Columna B ordenado de forma Ascendente");
-      informacion.sort(function(objetoA, objetoB)
-      { 
-        
-        //Ordenar por ID
-        return objetoA.columnab - objetoB.columnab;
-        
-        
-      });
-      console.log(informacion);
-    }
-    
+    //Link click event listener to the row
+    fila.addEventListener("click", (event) =>
+      HandlePersonClick(informacion[index - 1], event.target.parentElement)
+    );
   }
-  
-  
-  function ordenarB(){
-    if( selecciona.value === "participaciones"){
-      console.log("Participantes ordenado de forma Descendente");
-      informacion.sort(function(objetoB, objetoA)
-      { 
-        
+
+  btnOrdenarA.addEventListener("click", ordenarA);
+
+  btnOrdenarB.addEventListener("click", ordenarB);
+
+  function ordenarA() {
+    if (selecciona.value === "participaciones") {
+      informacion.sort(function (objetoA, objetoB) {
         //Ordenar por ID
-        
         return objetoA.participaciones - objetoB.participaciones;
-        
-        
       });
-      
-      console.log(informacion);
-      
-    }
-    else if( selecciona.value === "columnaA"){
-      console.log("Columna A ordenado de forma Descendente");
-      informacion.sort(function(objetoB, objetoA)
-      { 
-        
+    } else if (selecciona.value === "columnaA") {
+      informacion.sort(function (objetoA, objetoB) {
         //Ordenar por ID
         return objetoA.columnaa - objetoB.columnaa;
-        
-        
       });
-      console.log(informacion);
-      
-    }
-    else if( selecciona.value === "columnaB"){
-      console.log("Columna B ordenado de forma Descendente");
-      informacion.sort(function(objetoB, objetoA)
-      { 
-        
+    } else if (selecciona.value === "columnaB") {
+      informacion.sort(function (objetoA, objetoB) {
         //Ordenar por ID
         return objetoA.columnab - objetoB.columnab;
-        
-        
       });
-      console.log(informacion);
     }
-    
-  }    
+  }
+
+  function ordenarB() {
+    if (selecciona.value === "participaciones") {
+      informacion.sort(function (objetoB, objetoA) {
+        //Ordenar por ID
+
+        return objetoA.participaciones - objetoB.participaciones;
+      });
+    } else if (selecciona.value === "columnaA") {
+      informacion.sort(function (objetoB, objetoA) {
+        //Ordenar por ID
+        return objetoA.columnaa - objetoB.columnaa;
+      });
+    } else if (selecciona.value === "columnaB") {
+      informacion.sort(function (objetoB, objetoA) {
+        //Ordenar por ID
+        return objetoA.columnab - objetoB.columnab;
+      });
+    }
+  }
+
+  function HandlePersonClick(personClicked, personDOM) {
+    let person = {
+      personData: personClicked,
+      personDom: personDOM,
+    };
+
+    if (peopleSelected.length == 2) {
+      let oldestPerson = peopleSelected.shift();
+      oldestPerson.personDom.classList.remove("selected");
+    }
+
+    personDOM.classList.add("selected");
+    peopleSelected.push(person);
+
+    if (peopleSelected.length == 2)
+      SimilitudCoseno(
+        peopleSelected[0].personData,
+        peopleSelected[1].personData
+      );
+  } //closes HandlePersonClick method
+
+  function SimilitudCoseno(personA, personB) {
+    console.log(personA + ", " + personB);
+  } //closes SimilitudCoseno method
+
   body.appendChild(table);
-}
+} //closes sucessFunction method
